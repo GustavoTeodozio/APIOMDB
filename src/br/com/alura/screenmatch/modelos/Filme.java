e44bfe24e56651c502c9d9005a82cd49ec9a9e9f
Gustavo Teodozio <108893339+GustavoTeodozio@users.noreply.github.com>
2023-07-19 18:49:36 -0300

    Initial commit

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,29 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+
+
+    void exibiFichaTecnica () {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento do filme: " + anoDeLancamento);
+    }
+
+    void  avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+
+    }
+    double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
+
